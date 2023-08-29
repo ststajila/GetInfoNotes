d0e966b7d932e6ecb8e44bf7d965048351645bfd
Stanislav <stazilastas@gmail.com>
2023-08-29 09:30:40 -0500

    Initial Commit

---
+++ GetInfoNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetInfoNotes
+//
+//  Created by STANISLAV STAJILA on 8/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GetInfoNotesUITests/GetInfoNotesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GetInfoNotesUITestsLaunchTests.swift
+//  GetInfoNotesUITests
+//
+//  Created by STANISLAV STAJILA on 8/29/23.
+//
+
+import XCTest
+
+final class GetInfoNotesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
