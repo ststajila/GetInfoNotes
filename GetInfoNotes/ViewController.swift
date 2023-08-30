5f67bebdc36edc945a8f5a37bd0b933dda9570e1
Stanislav <stazilastas@gmail.com>
2023-08-30 09:30:42 -0500

    GetInfoNotes 1.0

--- GetInfoNotes/ViewController.swift
+++ GetInfoNotes/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelOutlet: UILabel!
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    @IBOutlet weak var ageOutlet: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func submitAction(_ sender: Any) {
+        
+        //Get text out of the Text field
+        var name = textFieldOutlet.text
+        var age = Int(ageOutlet.text!)
+        if let a = age{
+            var age10 = a + 10
+            
+            labelOutlet.text = "Hello \(name!) you will be \(age10) years old"
+        }
+    }
 }
 
